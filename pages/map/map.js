7dee133e6ecfc2402aac26f961c13bcf4ad77d87
NickCong <riwencong@yeah.net>
2017-06-21 12:07:37 +0800

    地图

--- pages/map/map.js
+++ pages/map/map.js
@@ -1,13 +1,20 @@
 // pages/map/map.js
 Page({
   data: {
-    markers: [{
+  btntitle:'快到碗里来',
+  markers: [{
       id: 0,
       latitude: 38.927720,
       longitude: 121.595457,
       width: 50,
       height: 50
     }]   
-  }
- 
+  },
+  onLoad: function () {
+    wx.openLocation({
+      latitude: 38.927720,
+      longitude: 121.595457,
+      scale: 16
+    })     
+  }, 
 })
